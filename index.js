420d0ca1611069af73fb3fe71386494031ae9d0e
Michael <mescamilla1114@gmail.com>
2022-09-03 14:21:13 -0500

    commit tree 21858600b28e444421f030b89c85b4d5fbe4c650

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name){
+const newappendCat = [...cats, "Broom"];
+return(newappendCat);
+}
+
+function prependCat(name){
+    const newprependCat = ["Arnold", ...cats];
+    return(newprependCat);
+}
+
+function removeLastCat (){
+const newLastCat = [...cats];
+newLastCat.pop();
+return(newLastCat);
+}
+
+function removeFirstCat (){
+    const newFirstCat = [...cats];
+    newFirstCat.shift();
+    return(newFirstCat);
+    }
